b1b38a537037c4c859cd0f84d9d93d84fc03a5ed
achithovhenrank01 <37116284+achithovhenrank01@users.noreply.github.com>
2024-04-28 22:27:27 +0800

    Update node.js

--- ulabolong/tai/asu/node.js
+++ ulabolong/tai/asu/node.js
@@ -1,21 +1,21 @@
-#EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="http://omzolix.elementfx.com/logoss/marhaban.png", MUSIK RELIGI 1
+#EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe1qDYdFrDJCa79uJ00TWJbVk-ECYMoSZCy7ewZDNbxn3H1L-wiJDGOIc&s=10", REPPA PENNE REPPA KACA
 https://achitho12.github.io/labacir01/hls/reppa_penne.m3u8
 
-#EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="http://omzolix.elementfx.com/logoss/marhaban.png", MUSIK RELIGI 2
+#EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe1qDYdFrDJCa79uJ00TWJbVk-ECYMoSZCy7ewZDNbxn3H1L-wiJDGOIc&s=10", BUGIS
 https://achitho12.github.io/labacir01/hls/bacir01/dash/bugis.m3u8
 
 
-#EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="http://omzolix.elementfx.com/logoss/marhaban.png", MUSIK RELIGI 3
+#EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe1qDYdFrDJCa79uJ00TWJbVk-ECYMoSZCy7ewZDNbxn3H1L-wiJDGOIc&s=10", BUGIS 1
 https://achitho12.github.io/labacir01/hls/bacir01/bugis1.m3u8
 
 
 
-#EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="http://omzolix.elementfx.com/logoss/marhaban.png", LEBARAN 1
+#EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe1qDYdFrDJCa79uJ00TWJbVk-ECYMoSZCy7ewZDNbxn3H1L-wiJDGOIc&s=10", PACCALAMMU
 https://achitho12.github.io/labacir01/hls/bacir01/dash/paccallamu.m3u8
 
 
 
-#EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="http://omzolix.elementfx.com/logoss/marhaban.png", LEBARAN 2
+#EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe1qDYdFrDJCa79uJ00TWJbVk-ECYMoSZCy7ewZDNbxn3H1L-wiJDGOIc&s=10", SUNGEKU WALA TANRO
 https://achitho12.github.io/labacir01/hls/bacir01/dash/sungeku_wala_tanro.m3u8
 
 
