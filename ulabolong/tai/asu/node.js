8c481984a7132ba0b391d473c2d154e94485e6d0
achithovhenrank01 <37116284+achithovhenrank01@users.noreply.github.com>
2024-04-28 22:23:09 +0800

    Update node.js

--- ulabolong/tai/asu/node.js
+++ ulabolong/tai/asu/node.js
@@ -1,22 +1,21 @@
 #EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="http://omzolix.elementfx.com/logoss/marhaban.png", MUSIK RELIGI 1
-https://www.dropbox.com/scl/fi/57ijvt50fahg2rj23mi2d/LAGU-RELIGI-ILAHILASTULIL-FIRDAUS-versi-KOPLO-TERBARU-2023.mp4?rlkey=ur1q1k7vcmavjr7iix48kj7sp&dl=1
-
+https://achitho12.github.io/labacir01/hls/reppa_penne.m3u8
 
 #EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="http://omzolix.elementfx.com/logoss/marhaban.png", MUSIK RELIGI 2
-https://www.dropbox.com/scl/fi/9qei5f1brrjgta71ydaa2/Lagu-Religi-Versi-Koplo-Cocok-Untuk-Santai-Voc.-Dewi-Ayunda.mp4?rlkey=s05nv1oaz7s2cms73ds1p0cal&dl=1
+https://achitho12.github.io/labacir01/hls/bacir01/dash/bugis.m3u8
 
 
 #EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="http://omzolix.elementfx.com/logoss/marhaban.png", MUSIK RELIGI 3
-https://www.dropbox.com/scl/fi/d5waq8tc4edcato8jmuv5/RAMADHAN-VERSI-AISYAH-PUJA-SYARMA.mp4?rlkey=zx14ettxh20132fiqfmrqjqxj&dl=1
+https://achitho12.github.io/labacir01/hls/bacir01/bugis1.m3u8
 
 
 
 #EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="http://omzolix.elementfx.com/logoss/marhaban.png", LEBARAN 1
-https://www.dropbox.com/scl/fi/55xtdrvu62s8s48anu46a/Gita-Gutawa-Idul-Fitri.mp4?rlkey=np46kkbvgpeite96bbqnccsgp&dl=1
+https://achitho12.github.io/labacir01/hls/bacir01/dash/paccallamu.m3u8
 
 
 
 #EXTINF:-1 group-title="PENYEJUK HATI" tvg-id="idul fitri" tvg-logo="http://omzolix.elementfx.com/logoss/marhaban.png", LEBARAN 2
-https://www.dropbox.com/scl/fi/nn6l2twi17jhl0a1rqryl/Hari-Lebaran-Ismail-Marzuki.mp4?rlkey=njlzjx40m1k2f9a69shbasum1&dl=1
+https://achitho12.github.io/labacir01/hls/bacir01/dash/sungeku_wala_tanro.m3u8
 
 
