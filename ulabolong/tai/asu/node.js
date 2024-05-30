604d5ec6c08edba22566ebaf02a7066d02bd8f08
achithovhenrank01 <37116284+achithovhenrank01@users.noreply.github.com>
2024-05-31 00:38:17 +0800

    Update node.js

--- ulabolong/tai/asu/node.js
+++ ulabolong/tai/asu/node.js
@@ -16,7 +16,7 @@
                     "referer": "https:\/\/www.dens.tv",
                     "logo_url": "https:\/\/i.ibb.co\/gmkpM80\/20240331-085144.jpg",
                     "name": "INDOSIAR",
-                    "stream_url": "http:\/\/op-group1-swiftservesd-1.dens.tv\/s\/s04\/index.m3u8?app_type=web&userid=lite&chname=Indosiar",
+                    "stream_url": "https:\/\/op-group1-swiftservesd-1.dens.tv\/s\/s04\/index.m3u8?app_type=web&userid=lite&chname=Indosiar",
                     "user_agent": "Mozilla\/5.0 (Linux; Android 9; CPH2083 Build\/PPR1.180610.011) AppleWebKit\/537.36"
                 },
                 {
@@ -24,7 +24,7 @@
                     "referer": "https:\/\/www.dens.tv",
                     "logo_url": "https:\/\/i.ibb.co\/cQLnjRP\/20240331-085221.jpg",
                     "name": "SCTV",
-                    "stream_url": "http:\/\/op-group1-swiftservehd-1.dens.tv\/s\/s03\/index.m3u8?app_type=web&userid=lite&chname=SCTV",
+                    "stream_url": "http:\/\/op-group1-swiftservehd-1.dens.tv\/h/h217\/02.m3u8",
                     "user_agent": "Mozilla\/5.0 (Linux; Android 9; CPH2083 Build\/PPR1.180610.011) AppleWebKit\/537.36"
                 },
                 {
